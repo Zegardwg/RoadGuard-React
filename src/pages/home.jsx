2149da3c0dae4bd2450cbb2722e0b8e5ca99c366
eggar <eggardewangga100@gmail.com>
2024-12-12 04:59:35 +0700

    update/dowlondlink

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -31,7 +31,7 @@ function Index() {
                   </li>
                   <li>
                     <a
-                      href="https://github.com/tailgrids/play-tailwind"
+                      href="https://github.com/Zegardwg"
                       target="_blank"
                       className="flex items-center gap-4 rounded-md bg-white/[0.12] px-6 py-[14px] text-base font-medium text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark">
                       <svg
